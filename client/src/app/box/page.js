7c9d09615853f8e018a5de5cc2d03c69e2af538a
Ajit Mandal <theajitmandal@gmail.com>
2024-05-23 22:03:19 +0545

    redux practiced and changed shape into complete circle

--- client/src/app/box/page.js
+++ client/src/app/box/page.js
@@ -1,13 +1,20 @@
 'use client'
+import { Button, Input } from '@nextui-org/react'
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector , useDispatch } from 'react-redux'
+import { changeHeight, changeWidth, changeShape, changeBackgroundColor } from '@/redux/reducerSlices/boxSlice'
 
 const Box = () => {
-    const {backgroundColor, height, width} = useSelector(state=>state.box)
+    const {backgroundColor, height, width, borderRadius} = useSelector(state=>state.box)
+    const dispatch = useDispatch()
   return (
     <div>
-        <div style={{backgroundColor: backgroundColor, height: height, width: width}}>
+        <div style={{backgroundColor: backgroundColor, height: height, width: width, borderRadius: borderRadius}}>
         </div>
+        <Button onClick={() => dispatch(changeWidth()) }>+Width</Button>
+        <Button onClick={() => dispatch(changeHeight()) }>+Height</Button>
+        <Button onClick={() => dispatch(changeShape()) }>Change to circle</Button>
+        <Input oncChange={(e) => dispatch(changeBackgroundColor(e.target.value))} placeholder="Enter color"/>
     </div>
   )
 }
--- client/src/redux/reducerSlices/boxSlice.js
+++ client/src/redux/reducerSlices/boxSlice.js
@@ -1,20 +1,44 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { 
-  backgroundColor: 'red',
+const initialState = {
+  backgroundColor: "red",
   width: 50,
   height: 50,
- }
+  borderRadius: "0%",
+};
 
 const boxSlice = createSlice({
-  name: 'box',
+  name: "box",
   initialState: initialState,
   reducers: {
     changeHeight(state) {
-      state.height++
+      state.height += 50;
+    },
+    changeWidth(state) {
+      state.width += 50;
+    },
+    // changeShape(state) {
+    //   if(state.borderRadius=== '0%'){
+    //     state.borderRadius = '50%'
+    //   }else if(state.borderRadius=== '50%'){
+    //     state.borderRadius = '0%'
+    //   }
+    // },
+    changeShape(state) {
+      // state.borderRadius === '0%' ? state.borderRadius = '50%' : state.borderRadius = '0%' ;
+      if (state.borderRadius === "0%") {
+        state.borderRadius = "50%";
+        state.height = state.width;
+      } else if (state.borderRadius === "50%") {
+        state.borderRadius = "0%"
+      }
+    },
+    changeBackgroundColor(state, actions) {
+      state.backgroundColor = actions.payload;
     },
   },
-})
+});
 
-export const { changeHeight } = boxSlice.actions
-export default boxSlice.reducer
+export const { changeHeight, changeWidth, changeShape, changeBackgroundColor } =
+  boxSlice.actions;
+export default boxSlice.reducer;
