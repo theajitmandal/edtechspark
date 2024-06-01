5228a6a49ceca4babb3613ea361108d290b89440
Ajit Mandal <theajitmandal@gmail.com>
2024-06-01 20:08:25 +0545

    Added dotenv code

--- server/index.js
+++ server/index.js
@@ -1,10 +1,45 @@
 const express = require('express')
 const app = express()
 const port = 4000
+require('dotenv').config()
 
-app.get('/', (req, res) => {
-  // res.send('Hello World!')
-  res.send(['hawkins', 'baltra'])
+// app.get('/me', (req, res) => {
+//   // res.send('Hello World!')
+//   res.send({
+//     name: "Ajit",
+//     balance: 100000,
+//     rewardPoint: 100
+//   })
+// })
+
+//query params
+// const userList = [
+//   'ajit', 'ram', 'mandal'
+// ]
+
+// app.get('/users', (req, res) => {
+//   const searchedUser = userList.filter((item) => {
+//     if(item[0] == req.query.startswith) return item
+//   })
+//   res.send(searchedUser)
+// })
+
+//path params
+const userList = [
+  {id:1, name: 'Ajit', addr: 'ktm'},
+  {id:2, name: 'Ram', addr: 'ktm'},
+  {id:3, name: 'Shyam', addr: 'brt'},
+  {id:4, name: 'Hari', addr: 'drn'},
+  {id:5, name: 'Hero', addr: 'pkr'},
+]
+
+app.get('/users/:id', (req, res) => {
+  const particularUser = userList.find((item) => {
+    if(item.id == req.params.id){
+      return item
+    }
+  })
+  res.send(particularUser)
 })
 
 app.listen(port, () => {
