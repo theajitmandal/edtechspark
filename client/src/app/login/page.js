d3f0fd29053e1ecfdd480dfc811740a69df5cf28
Ajit Mandal <theajitmandal@gmail.com>
2024-06-04 07:51:56 +0545

    Header Footer Added and signup form validated using formik yup

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -2,23 +2,29 @@ import React from "react";
 import { Input } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
 import Link from "next/link";
+import Header from "@/components/Header/page";
+import Footer from "@/components/Footer/page";
 
 export default function Login() {
   return (
-    <div className="flex flex-col mx-auto w-1/4 bg-white shadow shadow-lg text-center">
-      <h1 className="text-2xl text-bold text-center mt-2">
-        Login to your Account
-      </h1>
-      <div className="flex flex-col gap-4 m-5 p-2">
-        <Input type="email" label="Email" />
-        <Input type="password" label="Password" />
-        <Button color="primary">Login</Button>
-        <p>
-          <span className="text-lg text-sky-400">
-            <Link href="/">Forgot Password</Link>
-          </span>
-        </p>
+    <div>
+      <Header/>
+      <div className="flex flex-col mx-auto w-1/4 bg-white shadow shadow-lg text-center m-10">
+        <h1 className="text-2xl text-bold text-center mt-2">
+          Login to your Account
+        </h1>
+        <div className="flex flex-col gap-4 m-5 p-2">
+          <Input type="email" label="Email" />
+          <Input type="password" label="Password" />
+          <Button color="primary">Login</Button>
+          <p>
+            <span className="text-lg text-sky-400">
+              <Link href="/">Forgot Password</Link>
+            </span>
+          </p>
+        </div>
       </div>
+      <Footer/>
     </div>
   );
 }
--- client/src/app/register/page.js
+++ client/src/app/register/page.js
@@ -1,9 +1,12 @@
-"use client"
+"use client";
 import React from "react";
 import { Input } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
 import Link from "next/link";
 import { useFormik } from "formik";
+import * as Yup from "yup";
+import Header from "@/components/Header/page";
+import Footer from "@/components/Footer/page";
 
 const Register = () => {
   const formik = useFormik({
@@ -12,55 +15,69 @@ const Register = () => {
       email: "",
       password: "",
     },
+    validationSchema: Yup.object({
+      fullName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string()
+        .required("No Password Provided")
+        .min(8, "Password is too short - should be 8 chars minimum")
+        .matches(/[^\w]/, "Password must contain 1 special character"),
+    }),
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
   });
   return (
-    <div className="flex flex-col mx-auto w-1/4 bg-white shadow shadow-lg text-center">
-      <h1 className="text-2xl text-bold text-center mt-2">
-        Signup and Start Learning
-      </h1>
-      <div className="flex flex-col gap-4 m-5 p-2">
-        <form onSubmit={formik.handleSubmit} className="flex flex-col gap-4">
-          <Input
-            id="fullname"
-            name="fullname"
-            onChange={formik.handleChange}
-            value={formik.values.fullname}
-            isRequired
-            type="text"
-            label="Full Name"
-          />
-          <Input
-            id="email"
-            name="email"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-            isRequired
-            type="email"
-            label="Email"
-          />
-          <Input
-            id="password"
-            name="password"
-            onChange={formik.handleChange}
-            value={formik.values.password}
-            isRequired
-            type="password"
-            label="Password"
-          />
-          <Button color="primary">
-            Signup
-          </Button>
-        </form>
-        <p>
-          Already have an account?
-          <span className="text-lg text-sky-400">
-            <Link href="/login"> Login</Link>
-          </span>
-        </p>
+    <div>
+      <Header />
+      <div className="flex flex-col mx-auto w-1/4 bg-white shadow shadow-lg text-center m-10 p-5">
+        <h1 className="text-2xl text-bold text-center mt-2">
+          Signup and Start Learning
+        </h1>
+        <div className="flex flex-col gap-4 m-5 p-2">
+          <form onSubmit={formik.handleSubmit} className="flex flex-col gap-4">
+            <Input
+              id="fullname"
+              name="fullname"
+              onChange={formik.handleChange}
+              value={formik.values.fullname}
+              isRequired
+              type="text"
+              label="Full Name"
+            />
+
+            <Input
+              id="email"
+              name="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              isRequired
+              type="email"
+              label="Email"
+            />
+            <Input
+              id="password"
+              name="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              isRequired
+              type="password"
+              label="Password"
+            />
+            <Button color="primary">Signup</Button>
+          </form>
+
+          <p>
+            Already have an account?
+            <span className="text-lg text-sky-400">
+              <Link href="/login"> Login</Link>
+            </span>
+          </p>
+        </div>
       </div>
+      <Footer/>
     </div>
   );
 };
