4657bf498ec871b4f3e0090886b9ae41979913a0
Ajit Mandal <theajitmandal@gmail.com>
2025-04-11 21:20:50 +0545

    started

--- prac.js
+++ prac.js
@@ -1,28 +1,34 @@
-elements = [1, 2, 3, 4, 5]
-for(elements in elements){
-    console.log(elements);
-}
-const arr = [2, 4, 6, 8, 10]
-const newArr = arr.map((e) => e + 2 )
-const newArr1 = arr.filter((e) => e + 2 )
-console.log(newArr);
-console.log(newArr1);
+// elements = [1, 2, 3, 4, 5]
+// for(elements in elements){
+//     console.log(elements);
+// }
+// const arr = [2, 4, 6, 8, 10]
+// const newArr = arr.map((e) => e + 2 )
+// const newArr1 = arr.filter((e) => e + 2 )
+// console.log(newArr);
+// console.log(newArr1);
 
-const array = [2, 4, 6, 8]
-const array1 = [2, 4, 6, 8, 10, 12, 14, 16]
-const array3 = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 21, 23, 24]
-const array4 = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 21, 23, 24, 26]
-array.pop();
-array.push();
-array.unshift();
-console.log(`The new array is ${array}`);
-console.log(`The original array is ${array1}`);
-console.log(`The original array is ${array2}`);
-console.log(`The original array is ${array3}`);
-console.log(`The original array is ${array4}`);
-console.log(`The original array is ${array5}`);
-console.log(`The original array is ${array6}`);
-console.log(`The original array is ${array7}`);
-console.log(array);
+// const array = [2, 4, 6, 8]
+// const array1 = [2, 4, 6, 8, 10, 12, 14, 16]
+// const array3 = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 21, 23, 24]
+// const array4 = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 21, 23, 24, 26]
+// array.pop();
+// array.push();
+// array.unshift();
+// console.log(`The new array is ${array}`);
+// console.log(`The original array is ${array1}`);
+// console.log(`The original array is ${array2}`);
+// console.log(`The original array is ${array3}`);
+// console.log(`The original array is ${array4}`);
+// console.log(`The original array is ${array5}`);
+// console.log(`The original array is ${array6}`);
+// console.log(`The original array is ${array7}`);
+// console.log(array);
+
+
+const fruits = ["Apple", "Orange", "Apple", "Mango"];
+let position = fruits.indexOf("Apple") + 1;
+console.log([position]);
+
 
 
