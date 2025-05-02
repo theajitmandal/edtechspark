60fe361c35acff1f367db6361ec12221cb2d62cc
Ajit Mandal <theajitmandal@gmail.com>
2025-05-02 21:29:05 +0545

    signup page modified

--- prac.js
+++ prac.js
@@ -55,6 +55,29 @@ console.log(require(randomBytes));
 require('crypto').randomBytes(64).toString('hex')
 console.log(require('crypto').randomBytes(64).toString('hex'));
 
+switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
+
 
 
 
