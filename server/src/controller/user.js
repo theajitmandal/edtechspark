0a0d0c183baa2e72898f266a1e295ac09a9e26ac
Ajit Mandal <theajitmandal@gmail.com>
2025-05-07 20:44:05 +0545

    slight modificatio in backend

---
+++ server/src/controller/user.js
@@ -0,0 +1,61 @@
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+var jwt = require("jsonwebtoken");
+
+const findAllUsers = async (req, res) => {
+  try {
+    const data = await User.find();
+    res.json(data);
+  } catch (err) {
+    res.json({ msg: "Something went wrong" });
+  }
+};
+
+const registerUser = async (req, res) => {
+  console.log(req.body);
+  try {
+    const hashPassword = await bcrypt.hash(req.body?.password, saltRounds);
+    console.log(hashPassword);
+    req.body.password = hashPassword;
+    const phoneExist = await User.exists({ phone: req.body.phone });
+    const emailExist = await User.exists({ email: req.body.email });
+    
+    if (phoneExist) {
+      return res.status(409).json({ msg: "Phone Number already taken" });
+    } else if (emailExist) {
+      return res.status(409).json({ msg: "Email already taken" });
+    } else {
+      await User.create(req.body);
+      res.json({ msg: "User Created Successfully" });
+    }
+    console.log(hashPassword);
+  } catch (err) {
+    res.json({ msg: "Something went wrong" });
+  }
+};
+
+const logInUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ phone: req.body.phone });
+    if (user) {
+      const isMatched = await bcrypt.compare(req.body.password, user.password);
+      if (isMatched) {
+        const token = jwt.sign(
+          { phone: req.body.phone },
+          process.env.SECRET_KEY
+        );
+        console.log(process.env.SECRET_KEY);
+        res.json({ msg: "Aunthenticated", token, user });
+      } else {
+        res.status(401).json({ msg: "Invalid Password" });
+      }
+    } else {
+      res.status(401).json({ msg: "Phone Number not registered" });
+    }
+  } catch (err) {
+    res.json({ msg: "Something went wrong" });
+  }
+};
+
+module.exports = { findAllUsers, registerUser, logInUser }
